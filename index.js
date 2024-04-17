276a340c59623e21fc069eb4743b652f07972b7f
IZOTO-creator <ayotunderofiyat2003@gmail.com>
2024-04-16 23:36:44 -0700

    First push to github

---
+++ index.js
@@ -0,0 +1,6 @@
+let Name = "Rofiyat";
+let course = "Frontend Track";
+let channel = "Zuri Training";
+
+let info = `Hey there, I'm ${Name} by name and I'm currently learning ${course} with ${channel}`;
+console.log(info);
